e44abe2a58e19514f554de09837ef05afd75d63f
Tony de Padua Castor Alves <11017073481@tjpb.jus.br>
2020-04-02 19:52:39 -0300

    Enum OrderStatus

--- src/main/java/com/example/course/order/Order.java
+++ src/main/java/com/example/course/order/Order.java
@@ -2,15 +2,14 @@ package com.example.course.order;
 
 import com.example.course.user.User;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 @Entity
 @Table(name = "tb_order")
@@ -20,10 +19,33 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone ="GMT")
     private Instant moment;
+
     @ManyToOne
     @JoinColumn(name="client_id")
+
     private User client;
+
+    private Integer orderStatus;
+
+    public OrderStatus getOrderStatus(){
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus){
+        if(orderStatus !=null){
+            this.orderStatus=orderStatus.getCode();
+        }
+    }
+
+    public Order(Long id,Instant moment,User client,OrderStatus orderStatus) {
+        super();
+        this.id=id;
+        this.moment=moment;
+        this.client=client;
+        setOrderStatus(orderStatus);
+    }
 
 }
